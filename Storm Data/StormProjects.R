8841308001e49d4e8a7cdbadd6c642260272780a
Justin Wu <92274506+jwu1213@users.noreply.github.com>
2021-11-11 19:57:00 -0500

    Update StormProjects.R

--- Storm Data/StormProjects.R
+++ Storm Data/StormProjects.R
@@ -1,13 +1,11 @@
-######################## IN-PROGRESS #########################
 
-library(tidyverse)
-library(lubridate)
-#######read in data
 storms<-read_csv("StormEvents",col_types = cols(EPISODE_NARRATIVE="c",EVENT_NARRATIVE="c",WFO='c',EPISODE_ID='c',TOR_OTHER_CZ_STATE='c',SOURCE='c',BEGIN_LOCATION='c',END_LOCATION='c',TOR_OTHER_CZ_FIPS='c',TOR_OTHER_CZ_NAME='c',BEGIN_AZIMUTH='c',END_AZIMUTH='c',DAMAGE_PROPERTY="c",DAMAGE_CROPS='c',MAGNITUDE_TYPE='c',FLOOD_CAUSE='c',TOR_OTHER_WFO='c',CATEGORY='i'))
 View(storms)
 
 ######Aggregate
-x<-storms %>% group_by(YEAR,EVENT_TYPE) %>% summarise(count=n())
+x<-storms %>% 
+  group_by(YEAR,EVENT_TYPE) %>% 
+  summarise(count=n())
 
 #look at it:
 View(x)
@@ -52,6 +50,7 @@ storms <- storms %>%
 #Eyeball the result
 View(storms %>% select(DAMAGE_PROPERTY,prop_damage))
 unique(storms$STATE)
+View(storms)
 ###############
 #Plot1: Top Storms by State
 #Map
@@ -59,45 +58,150 @@ unique(storms$STATE)
 #Dim2: y 
 #Dim3: Color: Which storm
 `%notin%` <- Negate(`%in%`)
-nomap<-c("HAWAII","ALASKA","VIRGIN ISLANDS","PUERTO RICO", "RHODE ISLAND"       ,"GUAM","AMERICAN SAMOA","ATLANTIC SOUTH","LAKE MICHIGAN" ,"GULF OF MEXICO"     ,"ATLANTIC NORTH" ,"LAKE SUPERIOR","LAKE ST CLAIR","E PACIFIC","LAKE HURON"          ,"LAKE ERIE" ,"LAKE ONTARIO" ,"GULF OF ALASKA" ,"HAWAII WATERS","ST LAWRENCE R" )
+nomap<-c("HAWAII","ALASKA","VIRGIN ISLANDS","PUERTO RICO", 
+         "DISTRICT OF COLUMBIA","GUAM","AMERICAN SAMOA","ATLANTIC SOUTH",
+         "LAKE MICHIGAN", "GULF OF MEXICO","ATLANTIC NORTH",
+         "LAKE SUPERIOR", "LAKE ST CLAIR","E PACIFIC",
+         "LAKE HURON","LAKE ERIE", "LAKE ONTARIO" ,
+         "GULF OF ALASKA" ,"HAWAII WATERS","ST LAWRENCE R")
+
 top <- storms %>% 
+  filter(YEAR >= 2007)%>%
   filter(STATE %notin% nomap)%>%
   group_by(STATE,EVENT_TYPE) %>%
   summarise(Total = n())
 top$STATE<-tolower(top$STATE)
-  
+unique(top$STATE)
+
 top<-top %>%
   group_by(STATE) %>%
   slice_max(order_by = Total)
-View(top)
+
 us<-map_data("state")
 View(us)
+View(top)
+?map_data("state")
 
-plot_data <- us %>% 
-  left_join(storms, by=c("region"="STATE"))####BAD####
-plot_data
+map_data <- us %>% 
+  inner_join(top, by=c("region"="STATE"))
+View(map_data)
+unique(us$group)
+
+ggplot(map_data)+
+  geom_polygon(aes(long,lat,
+                   fill=EVENT_TYPE, group=group), 
+               color = "black")+
+  expand_limits(x=map_data$long, y=map_data$lat)+
+  ggtitle("Most Frequently Reported Storm Events per State (2007-2018)")
+  
+
 ###############
-#Plot2: Most Frequent Types of Storm + Most Destructive
+#Plot2: Most Frequent Types of Storm Per Season
 #Bar Chart
-#Dim1: x: type of storm
-#Dim2: y: Frequency
-#Dim3: Color: Damage
+s <- storms %>% select(YEAR,MONTH_NAME, EVENT_TYPE) %>% 
+  filter(YEAR > 2007)
+s
+s <- s %>% 
+  filter(EVENT_TYPE=="Heavy Snow"|
+           EVENT_TYPE=="High Wind"|
+           EVENT_TYPE=="Thunderstorm Wind" | 
+           EVENT_TYPE == "Hail")
+s
+spring <- c("March", "April", "May")
+summer <- c( "June", "July", "August")
+fall <- c("September","October", "November")
+winter <- c("December", "January", "Februrary")
 
+s$season <- s %>%
+  mutate(
+    season=case_when(
+      MONTH_NAME %in% spring ~ "Spring",
+      MONTH_NAME %in% summer ~ "Summer",
+      MONTH_NAME %in% fall ~ "Fall",
+      TRUE ~ "Winter"
+    )
+  )
+s$season
 
+ggplot(s$season) + 
+  geom_bar(aes(EVENT_TYPE, fill = season))+ 
+  theme(axis.text.x = element_text(hjust=.5))
+
+s
+
+str(s$season)
 
 ###############
-#Plot3: When is storm season?
+#Plot3:
 #Heatmap
-#Dim1: x: Month
-#Dim2: y: type of storm
-#Dim3: color: Average of each storm per month
+unique(storms$EVENT_TYPE)
+west <- c("california", "oregon", "washington", "montana", 
+         "wyoming", "colorado", "utah", "nevada", "idaho")
+southWest <- c( "arizona", "new mexico", "texas", "oklahoma")
+midWest <- c( "north dakota", "south dakota", "nebraska", 
+              "minnesota", "iowa", "missouri", "wisconsin", "michigan",
+              "illinois", "indiana", "ohio")
+southEast <- c("arkansas", "louisiana", "mississippi", "tennessee",
+               "alabama","kentucky", "west virginia", "maryland",
+               "virginia", "north carolina", "south carolina", "georgia",
+               "florida")
+northEast <- c("maine", "vermont", "new hampshire", "massachusetts", 
+               "new york", "rhode island", "connecticut", "new jersey",
+               "delaware", "pennsylvania")
+region.list <- list(West = west,
+                    `South West` = southWest,
+                    `Mid West` = midWest,
+                    `North East` = northEast,
+                    `South East` = southEast)
+winterEvents <- c("Ice Storm","Winter Weather","Heavy Snow","Winter Storm",
+                  "Cold/Wind Chill","Lake-Effect Snow","Sleet","Blizzard",
+                  "Avalanche","Frost/Freeze","Extreme Cold/Wind Chill",
+                  "Freezing Fog")
+region.list
+unique(season$EVENT_TYPE)
+#Setting up data
+#Filter states, weather events, and set to lowercase
+season <- storms %>%
+  select(STATE:EVENT_TYPE)%>%
+  filter(STATE %notin% nomap)%>%
+  filter(YEAR >= 2007)%>%
+  filter(EVENT_TYPE %in% winterEvents)
+season$STATE<-tolower(season$STATE)
+View(season)
+unique(season$EVENT_TYPE)
 
+#create region attribute
+season$regions <- sapply(season$STATE, 
+                         function(x) names(region.list)[grep(x,region.list)])
+str(season)
+season
 
+#group winter events by region and count how many
+heatmapData<-season %>% 
+  group_by(regions,MONTH_NAME) %>%
+  summarise(count=n())
 
+#as_date(heatmapData$MONTH_NAME)
+str(heatmapData)
+
+heatmapData$MONTH_NAME<-factor(heatmapData$MONTH_NAME,month.name)
+ggplot(heatmapData, 
+       aes(x=MONTH_NAME, y=regions, fill=count))+ 
+  geom_bin2d()#+
+  #scale_fill_viridis_c()
+
+View(heatmapData)
 ###############
-#Plot4: Duration of storm
-#Violin
-#Dim1: x: type of storm
-#Dim2: y: How long each storm lasts
-#Dim3: Color: total amount of each storm
-#Dim4: Width: Frequency of each time
+#Plot4:
+#lines
+lg <- storms %>% 
+  group_by(YEAR) %>% 
+  distinct() %>% 
+  count(EVENT_TYPE) %>% 
+  filter(EVENT_TYPE %in% c("Heavy Snow", "High Wind",
+                           "Thunderstorm Wind", "Hail")) %>% 
+  filter(YEAR > 2007)
+
+ggplot(lg, aes(YEAR, n, 
+               group = EVENT_TYPE, color = EVENT_TYPE))+
+  geom_line()
